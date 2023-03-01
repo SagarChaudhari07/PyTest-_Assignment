67918d74a04956ec7fd721d5d11a53ffff909ed1
Sagar Chaudhari <Sagarchaudhari7112000@gmail.com>
2023-03-01 17:57:34 +0530

    Adding two extra test cases

--- ToDo_test.py
+++ ToDo_test.py
@@ -3,6 +3,11 @@ import pytest
 from ToDo import app, db, Task
 
 @pytest.fixture(scope='module')
+def new_task():
+    task=Task(title='Task1', description='Task 1 description', due_date='2023-03-15',status='pending')
+    return task 
+
+@pytest.fixture(scope='module')
 def test_client():
     app.config['TESTING'] = True
     with app.test_client() as client:
@@ -20,7 +25,7 @@ def test_get_all_tasks(test_client):
 
 def test_add_task(test_client):
     task_data = {
-    'title': 'Test Task',
+    'title': 'Test_Task',
     'description': 'This is a test task.',
     'due_date': '2023-03-01'
     }
@@ -28,4 +33,27 @@ def test_add_task(test_client):
     assert response.status_code == 200
     assert response.json == {'status': 'success', 'message': 'Task added successfully'}
 
+def test_get_all_tasks_with_one_task(test_client, new_task):
+    db.session.add(new_task)
+    db.session.commit()
+    response = test_client.get('/getAll')
+    assert response.status_code == 200
+    assert len(response.json) == 2
+    assert response.json[0]['id'] == 1
+    assert response.json[0]['title'] == 'Test_Task'
+    assert response.json[0]['description'] == 'This is a test task.'
+    assert response.json[0]['due_date'] == '2023-03-01'
+    assert response.json[0]['status'] == 'pending'
+
+
+def test_add_new_task_with_missing_data(test_client):
+    task_data = {
+    'title': 'Task1',
+    # 'description': 'Missing Data Task',
+    # 'due_date': '2023-03-02'
+    }
+    response = test_client.post('/addTask', json=task_data)
+
+    assert response.status_code == 400
+    assert response.json['error'] == 'Bad Request'
 
