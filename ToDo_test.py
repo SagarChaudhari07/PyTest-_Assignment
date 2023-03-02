5da1cbe201618a59d5b98cfbbfda1320f17a945a
Sagar Chaudhari <Sagarchaudhari7112000@gmail.com>
2023-03-02 20:46:05 +0530

    Added two testcases

--- ToDo_test.py
+++ ToDo_test.py
@@ -4,21 +4,21 @@ from ToDo import app, db, Task
 
 @pytest.fixture(scope='module')
 def new_task():
-    task=Task(title='Task1', description='Task 1 description', due_date='2023-03-15',status='pending')
+    task=Task(title='Task1', description='Task 1 description', due_date='2023-03-15',status='pending') 
     return task 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope='module') #fixture sets the flask application configuration to testing (set in testing mode)
 def test_client():
-    app.config['TESTING'] = True
-    with app.test_client() as client:
-        with app.app_context():
-            db.create_all()
-            yield client
-        with app.app_context():
-            db.session.remove()
+    app.config['TESTING'] = True    #check this is exist or not in testing mode 
+    with app.test_client() as client:   # it will Only change in enviroment of database which will we do
+        with app.app_context():   #check the set enviroment it will right or wrong
+            db.create_all()       #fixture create a databse
+            yield client        #Return the Test_client object means change databsed
+        with app.app_context():  
+            db.session.remove()     #after the work it will remove the databse 
             db.drop_all()
 
-def test_get_all_tasks(test_client):
+def test_get_all_tasks(test_client):    #(test_client = create the database connection)
     response = test_client.get('/getAll')
     assert response.status_code == 200
     assert len(response.json) == 0
@@ -56,4 +56,29 @@ def test_add_new_task_with_missing_data(test_client):
 
     assert response.status_code == 400
     assert response.json['error'] == 'Bad Request'
+
+def test_get_task_by_id(test_client):
+    task = Task(title='Test Task', description='This is a test task.', due_date='2023-03-01', status='pending')
+    db.session.add(task)
+    db.session.commit()
+    response = test_client.get(f'/getdata/{task.id}')
+    assert response.status_code == 200
+    assert response.json['id'] == task.id
+
+def test_edit_task(test_client):
+    task = Task(title='Test Task', description='This is a test task.', due_date='2023-03-01', status='pending')
+    db.session.add(task)
+    db.session.commit()
+    updated_data = {
+    'title': 'Updated Task Title',
+    'description': 'This is an updated task description.',
+    'due_date': '2023-03-02'
+    }
+    response = test_client.put(f'/editTask/{task.id}', json=updated_data)
+    assert response.status_code == 200
+    assert response.json == {'status': 'success', 'message': 'Task updated successfully'}
+    updated_task = Task.query.get(task.id)
+    assert updated_task.title == updated_data['title']
+    assert updated_task.description == updated_data['description']
+    assert str(updated_task.due_date) == updated_data['due_date']
 
